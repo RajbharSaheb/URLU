99cfa68bcd4d8b091ee1d758c489c98e2b9eabf3
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-01-01 14:07:50 +0530

    Update ipinfo.py

--- plugins/ipinfo.py
+++ plugins/ipinfo.py
@@ -12,10 +12,10 @@ command = partial(filters.command, prefixes=["!", "/", "."])
 
 
 @Ntbot.on_message(command("ipinfo"))
-async def ipinfo(_, message): 
+async def ipinfo(_, message: Message):
     searchip = message.text.split(" ", 1)
     if len(searchip) == 1:
-        await message.reply_text("**Usage:**\n/ipinfo [ip]")
+        await message.reply_text("Usage:\n/ipinfo [ip]")
         return
     else:
         searchip = searchip[1]
@@ -24,29 +24,29 @@ async def ipinfo(_, message):
         url = requests.get(f"http://ip-api.com/json/{searchip}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query")
         response = json.loads(url.text)
         text = f"""
-**IP Address:** `{response['query']}`
-**Status:** `{response['status']}`
-**Continent Code:** `{response['continentCode']}`
-**Country:** `{response['country']}`
-**Country Code :** `{response['countryCode']}`
-**Region:** `{response['region']}`
-**Region Name :** `{response['regionName']}`
-**City:** `{response['city']}`
-**District:** `{response['district']}`
-**ZIP:** `{response['zip']}`
-**Latitude:** `{response['lat']}`
-**Longitude:** `{response['lon']}`
-**Time Zone:** `{response['timezone']}`
-**Offset:** `{response['offset']}`
-**Currency:** `{response['currency']}`
-**ISP:** `{response['isp']}`
-**Org:** `{response['org']}`
-**As:** `{response['as']}`
-**Asname:** `{response['asname']}`
-**Reverse:** `{response['reverse']}`
-**User is on Mobile:** `{response['mobile']}`
-**Proxy:** `{response['proxy']}`
-**Hosting:** `{response['hosting']}`"""
+IP Address: {response['query']}
+Status: {response['status']}
+Continent Code: {response['continentCode']}
+Country: {response['country']}
+Country Code : {response['countryCode']}
+Region: {response['region']}
+Region Name : {response['regionName']}
+City: {response['city']}
+District: {response['district']}
+ZIP: {response['zip']}
+Latitude: {response['lat']}
+Longitude: {response['lon']}
+Time Zone: {response['timezone']}
+Offset: {response['offset']}
+Currency: {response['currency']}
+ISP: {response['isp']}
+Org: {response['org']}
+As: {response['as']}
+Asname: {response['asname']}
+Reverse: {response['reverse']}
+User is on Mobile: {response['mobile']}
+Proxy: {response['proxy']}
+Hosting: {response['hosting']}"""
         await m.edit_text(text, parse_mode="markdown")
     except:
         await m.edit_text("Unable To Find Info!")
