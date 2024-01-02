b71da5e3f1cd392ace7c23b086f55b09a37cba05
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-01-02 08:53:51 +0530

    Update info.py

--- plugins/info.py
+++ plugins/info.py
@@ -13,6 +13,8 @@ def is_enabled(value, default):
 
 
 
+LOG_GROUP = "-1001306080448" # Group ID for the log channel or leave it empty if not required 
+BUG = "-1001306080448" #add your group id for getting error log messages or leave it empty if not required 
 
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>🎬 Title:</b> <a href={url}>{title}</a> [{year}] —<b>{kind}</b>\n\n<b>📆 Release:</b> <a href={url}/releaseinfo>{release_date}</a>\n<b>🌟 Rating:</b> <a href={url}/ratings>{rating} / 10</a>\n(based on <code>{votes}</code> user ratings.)\n\n<b>🎭 Genres:</b> #{genres}\n<b>📀 Runtime:</b> <code>{runtime} minutes</code>\n\n<b>☀️ Languages:</b> #{languages}\n<b>🌎 Country of Origin:</b> #{countries}\n<b>🎥 Director:</b> {director}\n\n<b><a href='https://t.me/omg_info'>© IMDb (Series & Movie) Studio</a></b>\n\n<b>✍️ Note:</b> <s>This message will be Auto-deleted after 10 hours to avoid copyright issues.</s>")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
