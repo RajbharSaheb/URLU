cb062a0e0dd3c82ef52a7ebce1b95093183b0aca
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-01-08 17:51:31 +0530

    Update Savan.py

--- plugins/Savan.py
+++ plugins/Savan.py
@@ -1,7 +1,7 @@
 ### This download from saavn.me an unofficial api
 from pyrogram import Client,filters
 import requests,os,wget 
-@Client.on_message(filters.command('saavn') & filters.text)
+@@Client.on_message(filters.private & filters.command('saavn'))
 async def song(client, message):
     try:
        args = message.text.split(None, 1)[1]
