7c153ec102a41f66fec2cdb2ff080edb13922257
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-01-09 11:28:30 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -50,14 +50,14 @@ async def echo(bot, update):
         return await update.reply_text("I don't know about you sar :(")
     await add_user_to_database(bot, update)
     
-await bot.send_chat_action(
-       chat_id=update.effective_chat.id,
-       action="typing"
+    await bot.send_chat_action(
+        chat_id=update.effective_chat.id,
+        action="typing"
     )
     if Config.UPDATES_CHANNEL:
-      fsub = await handle_force_subscribe(bot, update)
-      if fsub == 400:
-        return
+        fsub = await handle_force_subscribe(bot, update)
+        if fsub == 400:
+            return
     logger.info(update.from_user.name)
     url = update.text
     youtube_dl_username = None
