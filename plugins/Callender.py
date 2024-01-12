7cc5b43d898375f54c7e499fbe1af3227644750d
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-01-12 16:27:10 +0530

    Update Callender.py

--- plugins/Callender.py
+++ plugins/Callender.py
@@ -1,44 +1,10 @@
+from pyrogram import Client, filters
 
-import calendar
-import time
-import os
-from pyrogram import Client, filters, emoji
-
-@Client.on_message(filters.command(["Calendar"]))
-def print_calendar(year):
-  """Prints a calendar for the given year."""
-
-  # Get the first and last day of the year.
-  first_day = calendar.datetime(year, 1, 1)
-  last_day = calendar.datetime(year, 12, 31)
-
-  # Get the number of days in the year.
-  num_days = (last_day - first_day).days + 1
-
-  # Get the number of weeks in the year.
-  num_weeks = calendar.monthrange(year, 1)[1]
-
-  # Create a list of the days of the year.
-  days = []
-  for i in range(num_days):
-    day = first_day + timedelta(days=i)
-    days.append(day)
-
-  # Create a list of the weeks of the year.
-  weeks = []
-  for i in range(num_weeks):
-    week = days[i * 7:(i + 1) * 7]
-    weeks.append(week)
-
-  # Print the calendar.
-  for week in weeks:
-    for day in week:
-      print(day.strftime("%Y-%m-%d"), end=" ")
-    print()
-
-if __name__ == "main":
-  # Get the year from the user.
-  year = int(input("Enter a year: "))
-
-  # Print the calendar.
-  print_calendar(year)
+@Client.on_message(filters.command("calendar"))
+async def calendar(client, message):
+    try:
+        today = datetime.today()
+        input_ = calendar.month(today.year, today.month)
+        await message.reply_text(f"{input_}")
+    except Exception as err:
+        await message.reply_text("Exception Occured:- " + str(err))
