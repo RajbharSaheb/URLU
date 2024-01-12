62679cdb9d8369762aac723af77967ae89667581
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-01-12 17:25:04 +0530

    Update Callender.py

--- plugins/Callender.py
+++ plugins/Callender.py
@@ -7,7 +7,7 @@ from datetime import datetime
 async def calendar(client, message):
     try:
         today = datetime.today()
-        input_ = calendar.month(today.year, today.month)
+        input_ = calendar.month(today.year, today.month)  # Use calendar.month() function
         await message.reply_text(f"{input_}")
     except Exception as err:
         await message.reply_text("Exception Occured:- " + str(err))
