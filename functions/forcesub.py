a478a7a490540621905838acda4d9ef4035f340f
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-01-17 11:17:57 +0530

    Update forcesub.py

--- functions/forcesub.py
+++ functions/forcesub.py
@@ -6,23 +6,23 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 async def handle_force_subscribe(bot, message):
     try:
-        invite_link = await bot.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
+        invite_link = await Client.create_chat_invite_link(int(Config.UPDATES_CHANNEL))
     except FloodWait as e:
         await asyncio.sleep(e.x)
         return 400
     try:
-        user = await bot.get_chat_member(int(Config.UPDATES_CHANNEL), message.from_user.id)
+        user = await Client.get_chat_member(int(Config.UPDATES_CHANNEL), message.from_user.id)
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=message.from_user.id,
                 text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/shado_hackers).",
                 parse_mode="markdown",
                 disable_web_page_preview=True,
-                reply_to_message_id=message.message_id,
+                reply_to_message_id=message.id,
             )
             return 400
     except UserNotParticipant:
-        await bot.send_message(
+        await Client.send_message(
             chat_id=message.from_user.id,
             text="**Please Join My Update Channel To Use Me**",
             reply_markup=InlineKeyboardMarkup(
@@ -33,16 +33,16 @@ async def handle_force_subscribe(bot, message):
                 ]
             ),
             parse_mode="markdown",
-            reply_to_message_id=message.message_id,
+            reply_to_message_id=message.id,
         )
         return 400
     except Exception:
-        await bot.send_message(
+        await Client.send_message(
             chat_id=message.from_user.id,
             text="Something Went Wrong. Contact My [Support Group](https://t.me/shado_hackers).",
             parse_mode="markdown",
             disable_web_page_preview=True,
-            reply_to_message_id=message.message_id,
+           reply_to_message_id=message.id,
         )
         return 400
 
