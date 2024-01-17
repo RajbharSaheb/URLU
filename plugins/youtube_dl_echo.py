11bf28b428de23dc5e3e38820175b4bac57da013
LUSIFER <70184956+shado-hackers@users.noreply.github.com>
2024-01-17 10:22:05 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -122,16 +122,12 @@ async def echo(bot, update):
         command_to_exec.append("--password")
         command_to_exec.append(youtube_dl_password)
     logger.info(command_to_exec)
-    #   def send_analysis_message(message):
-       chk = await client.send_message(
+    chk = await client.send_message(
                 chat_id=message.chat.id,
                 text=f'<b>Analysing url.... </b>',
                 disable_web_page_preview=True,
                 reply_to_message_id=message.id
             )
-   # Call the function and pass the message object
-  # send_analysis_message(message)
-   
     process = await asyncio.create_subprocess_exec(
         *command_to_exec,
         # stdout must a pipe to be accessible as process.stdout
