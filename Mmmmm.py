3b60898245d1cdafab646a4a99ca9d535aef10a9
NOTHING <70184956+shado-hackers@users.noreply.github.com>
2024-06-03 09:19:42 +0530

    Create Mmmmm.py

---
+++ Mmmmm.py
@@ -0,0 +1,14 @@
+HELP_BUTTONS =InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Mise", callback_data="mise"),
+                        InlineKeyboardButton("imgedit", callback_data="imgeedit"),
+                        InlineKeyboardButton("search", callback_data="search"),
+                    ],
+                    [
+                        InlineKeyboardButton("paste", callback_data="paste"),
+                        InlineKeyboardButton("extra", callback_data="extra"),
+                    ],
+                ]
+)
+               
